8533961fc39485550d2cab4a57e3ba98e6f35df2
imoralesgt <ivan.rene.morales@gmail.com>
2014-09-28 17:00:41 -0600

    User I/O & Variables

---
+++ CLASES/holaMundo2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int n; //Variable - Numero Entero
+
+int main(void){
+	printf("Ingrese un numero: ");
+	scanf("%d", &n); //Leer lo que el usuario ingresa con el teclado
+	printf("Numero ingresado: %d", n); //Desplegar una variable tipo entero
+	return 0;
+}
+
+
